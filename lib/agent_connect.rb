8c0ceea81fb2af7bcb2b8103470d0945e1b4bc3d
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-08-07 14:06:21 +0200

    add ability to set an error callback

--- lib/agent_connect.rb
+++ lib/agent_connect.rb
@@ -3,12 +3,20 @@ require "agent_connect/engine"
 require "openid_connect"
 
 module AgentConnect
-  mattr_accessor :client_id, :client_secret, :scope, :base_url, :success_callback, :error_callback, :discovery, :algorithm
+  mattr_accessor :client_id, :client_secret, :scope, :base_url, :success_callback, :error_callback, :discovery, :algorithm,
+                  :bootstrap_error_callback
 
   class << self
     def initialize!
       yield(self)
-      self.discovery = OpenIDConnect::Discovery::Provider::Config.discover!(base_url)
+
+      begin
+        self.discovery = OpenIDConnect::Discovery::Provider::Config.discover!(base_url)
+      rescue StandardError => e
+        raise e unless bootstrap_error_callback.present?
+
+        bootstrap_error_callback.call(e)
+      end
     end
   end
 end
